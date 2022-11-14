68d655c4c4bb62a4103217e064f3aca3a34edb24
Nikita Bulavin <nik.bulavin.005@gmail.com>
2022-11-14 11:50:19 +0300

    Add Material3 Theme

--- app/src/main/java/ru/resodostudios/movies/presentation/screens/splash/components/Splash.kt
+++ app/src/main/java/ru/resodostudios/movies/presentation/screens/splash/components/Splash.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.PlayArrow
-import androidx.compose.material.icons.outlined.PlayCircleOutline
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
