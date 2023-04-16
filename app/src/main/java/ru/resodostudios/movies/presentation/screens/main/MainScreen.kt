82812e4ac0384b4c2458258d47b97b23871c93cc
Nikita Bulavin <nik.bulavin.005@gmail.com>
2023-04-16 19:53:51 +0300

    Add Menu IconButton to SearchBar

--- app/src/main/java/ru/resodostudios/movies/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/ru/resodostudios/movies/presentation/screens/main/MainScreen.kt
@@ -1,48 +1,24 @@
 package ru.resodostudios.movies.presentation.screens.main
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.isContainer
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import ru.resodostudios.movies.presentation.navigation.Screens
 import ru.resodostudios.movies.presentation.screens.main.components.MovieCard
+import ru.resodostudios.movies.presentation.screens.main.components.SearchBar
 
 @ExperimentalMaterial3Api
 @Composable
@@ -51,91 +27,25 @@ fun MainScreen(
     viewModel: MainViewModel = hiltViewModel()
 ) {
 
-    val movies = viewModel.movies.observeAsState(listOf()).value
-
-    var text by rememberSaveable { mutableStateOf("") }
-    var active by rememberSaveable { mutableStateOf(false) }
+    val movies by viewModel.movies.observeAsState(listOf())
 
     Surface(Modifier.fillMaxSize()) {
-        Box(
-            Modifier
-                .semantics { isContainer = true }
-                .zIndex(1f)
-                .fillMaxWidth()
-        ) {
-            SearchBar(
-                modifier = Modifier.align(Alignment.TopCenter),
-                query = text,
-                onQueryChange = {
-                    text = it
-                    viewModel.searchMovie(it)
-                },
-                onSearch = { active = false },
-                active = active,
-                onActiveChange = { active = it },
-                placeholder = { Text("Search movies") },
-                leadingIcon = {
-                    if (active) {
-                        IconButton(onClick = { active = false }) {
-                            Icon(Icons.Default.ArrowBack, contentDescription = null)
-                        }
-                    } else {
-                        IconButton(onClick = {  }) {
-                            Icon(Icons.Default.Menu, contentDescription = null)
-                        }
-                    }
-                },
-                trailingIcon = {
-                    if (active) {
-                        IconButton(
-                            onClick = {
-                                if (text.isNotBlank()) {
-                                    text = ""
-                                    viewModel.searchMovie("")
-                                }
-                            }
-                        ) {
-                            Icon(Icons.Default.Close, contentDescription = null)
-                        }
-                    }
-                },
-                windowInsets = WindowInsets.statusBars
-            ) {
-                LazyColumn(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentPadding = PaddingValues(16.dp),
-                    verticalArrangement = Arrangement.spacedBy(4.dp)
-                ) {
-                    if (text.isNotBlank()) {
-                        items(movies) { movie ->
-                            ListItem(
-                                headlineContent = { Text(movie.name) },
-                                supportingContent = {
-                                    Row {
-                                        movie.genres.take(2).forEach {
-                                            Text(text = "$it ")
-                                        }
-                                    }
-                                },
-                                leadingContent = { Icon(Icons.Filled.Search, contentDescription = null) },
-                                modifier = Modifier.clickable {
-                                    navController.navigate(Screens.Movie.route + "/${movie.id}")
-                                    active = false
-                                }
-                            )
-                        }
-                    }
-                }
-            }
-        }
+        SearchBar(
+            viewModel = viewModel,
+            movies = movies,
+            navController = navController
+        )
 
         LazyColumn(
             modifier = Modifier.padding(start = 16.dp, end = 16.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             item {
-                Spacer(modifier = Modifier.statusBarsPadding())
-                Spacer(modifier = Modifier.requiredHeight(70.dp))
+                Spacer(
+                    modifier = Modifier
+                        .statusBarsPadding()
+                        .requiredHeight(70.dp)
+                )
             }
             items(movies) { movie ->
                 MovieCard(movie = movie, navController = navController)
