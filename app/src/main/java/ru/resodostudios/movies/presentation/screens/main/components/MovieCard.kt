3be3128ed714da5c07acacb97e9275a36852e4b1
Nikita Bulavin <nik.bulavin.005@gmail.com>
2023-04-23 20:06:12 +0300

    Update MovieCard

--- app/src/main/java/ru/resodostudios/movies/presentation/screens/main/components/MovieCard.kt
+++ app/src/main/java/ru/resodostudios/movies/presentation/screens/main/components/MovieCard.kt
@@ -4,12 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material.icons.outlined.StarRate
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -17,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import ru.resodostudios.movies.data.models.Movie
@@ -29,7 +31,8 @@ import ru.resodostudios.movies.presentation.ui.theme.Typography
 fun MovieCard(movie: Movie, navController: NavController) {
 
     Card(
-        onClick = { navController.navigate(Screens.Movie.route + "/${movie.id}") }
+        onClick = { navController.navigate(Screens.Movie.route + "/${movie.id}") },
+        shape = RoundedCornerShape(16.dp)
     ) {
         Row(
             modifier = Modifier
@@ -38,21 +41,20 @@ fun MovieCard(movie: Movie, navController: NavController) {
         ) {
             CoilImage(
                 url = movie.image.medium,
-                height = 128.dp,
-                width = 91.dp
+                height = 98.dp,
+                width = 70.dp
             )
 
             Spacer(modifier = Modifier.width(16.dp))
 
             Column(
-                modifier = Modifier.height(128.dp),
                 content = {
                     Text(
                         text = movie.name,
                         style = Typography.titleLarge
                     )
 
-                    Spacer(modifier = Modifier.height(8.dp))
+                    Spacer(modifier = Modifier.requiredHeight(4.dp))
 
                     Row(verticalAlignment = Alignment.CenterVertically) {
                         Text(
@@ -61,29 +63,16 @@ fun MovieCard(movie: Movie, navController: NavController) {
                         )
 
                         Icon(
-                            modifier = Modifier.size(20.dp),
-                            imageVector = Icons.Rounded.Star,
-                            contentDescription = "Rating"
+                            modifier = Modifier.size(14.dp),
+                            imageVector = Icons.Outlined.StarRate,
+                            contentDescription = "Star"
                         )
                     }
 
-                    Row {
-                        movie.genres.take(2).forEach {
-                            Text(
-                                text = "$it ",
-                                style = Typography.bodyMedium
-                            )
-                        }
-                    }
-
                     Text(
-                        text = movie.language,
-                        style = Typography.bodyMedium
-                    )
-
-                    Text(
-                        text = movie.premiered,
-                        style = Typography.bodyMedium
+                        text = movie.genres[0],
+                        style = Typography.bodyMedium,
+                        textAlign = TextAlign.Start
                     )
                 }
             )
