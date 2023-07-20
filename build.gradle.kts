0deed3d00cf8eee7fc9e3f42703b1fca30c82c95
Nikita Bulavin <nik.bulavin.005@gmail.com>
2023-07-20 09:50:47 +0300

    Update Kotlin Version
    1.8.22 -> 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.2.0-alpha13" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.dagger.hilt.android") version "2.47" apply false
-    kotlin("plugin.serialization") version "1.8.22" apply false
-    id("com.google.devtools.ksp") version "1.8.22-1.0.11" apply false
+    kotlin("plugin.serialization") version "1.9.0" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.11" apply false
 }
