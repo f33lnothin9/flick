d2d4543860e607708f0a29294dc183035fd97bdd
Nikita Bulavin <f33lnothin9@proton.me>
2023-08-03 10:46:03 +0300

    Add Lottie Animation to SettingsScreen

--- app/src/main/java/ru/resodostudios/flick/feature/settings/presentation/SettingsScreen.kt
+++ app/src/main/java/ru/resodostudios/flick/feature/settings/presentation/SettingsScreen.kt
@@ -1,5 +1,10 @@
 package ru.resodostudios.flick.feature.settings.presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
@@ -7,18 +12,31 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import ru.resodostudios.flick.R
 
 @ExperimentalMaterial3Api
 @Composable
 fun SettingsScreen(onBack: () -> Unit) {
 
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val lottieComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.anim_dev))
+    val progress by animateLottieCompositionAsState(composition = lottieComposition, iterations = LottieConstants.IterateForever)
 
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -34,6 +52,25 @@ fun SettingsScreen(onBack: () -> Unit) {
             )
         }
     ) { innerPadding ->
+
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                LottieAnimation(
+                    modifier = Modifier.size(256.dp),
+                    composition = lottieComposition,
+                    contentScale = ContentScale.Fit,
+                    progress = { progress }
+                )
+                Text(
+                    text = "Work in Progress",
+                    maxLines = 1,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
 
         LazyColumn(
             contentPadding = innerPadding
