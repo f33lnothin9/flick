d9a46492f55e20942116641e0df3482b6f85c649
Nikita Bulavin <f33lnothin9@proton.me>
2023-08-13 19:57:14 +0300

    Enable EdgeToEdge

--- app/src/main/java/ru/resodostudios/flick/core/presentation/MainActivity.kt
+++ app/src/main/java/ru/resodostudios/flick/core/presentation/MainActivity.kt
@@ -3,17 +3,17 @@ package ru.resodostudios.flick.core.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.rememberDrawerState
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import ru.resodostudios.flick.core.presentation.navigation.NavHost
 import ru.resodostudios.flick.core.presentation.components.NavDrawer
+import ru.resodostudios.flick.core.presentation.navigation.NavHost
 import ru.resodostudios.flick.core.presentation.theme.MoviesTheme
 
 @ExperimentalFoundationApi
@@ -22,9 +22,9 @@ import ru.resodostudios.flick.core.presentation.theme.MoviesTheme
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        enableEdgeToEdge()
         setContent {
             MoviesTheme {
                 val navController = rememberNavController()
