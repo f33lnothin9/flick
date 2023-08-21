2273e779ac193f0ee701733d71887c71a6d0ae32
Nikita Bulavin <f33lnothin9@proton.me>
2023-08-21 17:54:46 +0300

    Delete Constants.kt

--- app/src/main/java/ru/resodostudios/flick/core/Constants.kt
+++
@@ -1,9 +0,0 @@
-package ru.resodostudios.flick.core
-
-object Constants {
-    const val MOVIES_SCREEN = "movies_screen"
-    const val MOVIE_SCREEN = "movie_screen"
-    const val FAVORITES_SCREEN = "favorites_screen"
-    const val SETTINGS_SCREEN = "settings_screen"
-    const val PEOPLE_SCREEN = "people_screen"
-}
