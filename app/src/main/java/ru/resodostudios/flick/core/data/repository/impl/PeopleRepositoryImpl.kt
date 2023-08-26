c7637818285b812b5f74adb8f3db7932c4c0b278
Nikita Bulavin <f33lnothin9@proton.me>
2023-08-26 11:37:36 +0300

    Add NetworkPerson asExternalModel map function

--- app/src/main/java/ru/resodostudios/flick/core/data/repository/impl/PeopleRepositoryImpl.kt
+++ app/src/main/java/ru/resodostudios/flick/core/data/repository/impl/PeopleRepositoryImpl.kt
@@ -7,10 +7,9 @@ import kotlinx.coroutines.flow.flowOn
 import ru.resodostudios.flick.core.common.network.Dispatcher
 import ru.resodostudios.flick.core.common.network.FlickDispatchers.IO
 import ru.resodostudios.flick.core.data.repository.PeopleRepository
-import ru.resodostudios.flick.core.model.data.Country
-import ru.resodostudios.flick.core.model.data.Image
 import ru.resodostudios.flick.core.model.data.Person
 import ru.resodostudios.flick.core.network.FlickNetworkDataSource
+import ru.resodostudios.flick.core.network.model.asExternalModel
 import javax.inject.Inject
 
 class PeopleRepositoryImpl @Inject constructor(
@@ -22,24 +21,7 @@ class PeopleRepositoryImpl @Inject constructor(
         emit(
             datasource
                 .getPeople()
-                .map {
-                    Person(
-                        id = it.id,
-                        birthday = it.birthday,
-                        country = Country(
-                            name = it.country.name,
-                            code = it.country.code,
-                            timezone = it.country.timezone
-                        ),
-                        gender = it.gender,
-                        deathday = it.deathday,
-                        image = Image(
-                            medium = it.image.medium,
-                            original = it.image.original
-                        ),
-                        name = it.name
-                    )
-                }
+                .map { it.asExternalModel() }
         )
     }.flowOn(ioDispatcher)
 }
--- app/src/main/java/ru/resodostudios/flick/core/network/model/NetworkPerson.kt
+++ app/src/main/java/ru/resodostudios/flick/core/network/model/NetworkPerson.kt
@@ -1,6 +1,9 @@
 package ru.resodostudios.flick.core.network.model
 
 import kotlinx.serialization.Serializable
+import ru.resodostudios.flick.core.model.data.Country
+import ru.resodostudios.flick.core.model.data.Image
+import ru.resodostudios.flick.core.model.data.Person
 
 @Serializable
 data class NetworkPerson(
@@ -11,4 +14,21 @@ data class NetworkPerson(
     val id: Int = 0,
     val image: NetworkImage = NetworkImage(),
     val name: String = ""
+)
+
+fun NetworkPerson.asExternalModel() = Person(
+    id = id,
+    birthday = birthday,
+    country = Country(
+        name = country.name,
+        code = country.code,
+        timezone = country.timezone
+    ),
+    gender = gender,
+    deathday = deathday,
+    image = Image(
+        medium = image.medium,
+        original = image.original
+    ),
+    name = name
 )
