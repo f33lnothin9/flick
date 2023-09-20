ee3d9582c08cc736b3f378b9f0743c0f41c041d9
Nikita Bulavin <f33lnothin9@proton.me>
2023-09-20 14:52:57 +0300

    Add PersonBody with Cast Credits

--- app/src/main/java/ru/resodostudios/flick/feature/person/PersonScreen.kt
+++ app/src/main/java/ru/resodostudios/flick/feature/person/PersonScreen.kt
@@ -1,6 +1,7 @@
 package ru.resodostudios.flick.feature.person
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,22 +11,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.waterfall
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -139,15 +149,18 @@ private fun PersonHeader(person: Person) {
                 fontWeight = FontWeight.Bold
             )
 
-            Column {
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
                 Text(
-                    text = person.country.name + ", ${person.country.code} ",
+                    text = person.country.name + ", ${person.country.code}",
                     style = Typography.labelLarge,
                     textAlign = TextAlign.Start
                 )
 
                 Text(
-                    text = person.birthday + " - " + person.deathday,
+                    text = person.birthday + " - ${person.deathday}",
                     style = Typography.labelLarge,
                     textAlign = TextAlign.Start
                 )
@@ -171,13 +184,47 @@ private fun PersonBody(personExtended: PersonExtended) {
     Column(
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier.padding(16.dp)
-        ) {
+        var castSize by rememberSaveable { mutableIntStateOf(288) }
 
+        if (personExtended.castCredits.size < 4 && personExtended.castCredits.isNotEmpty()) castSize =
+            72 * personExtended.castCredits.size
+
+        if (personExtended.castCredits.isNotEmpty()) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(bottom = 16.dp, top = 8.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.cast_credits),
+                    style = Typography.titleMedium,
+                    color = MaterialTheme.colorScheme.secondary,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+
+                LazyHorizontalGrid(
+                    rows = GridCells.Adaptive(72.dp),
+                    modifier = Modifier.height(castSize.dp)
+                ) {
+                    items(personExtended.castCredits) { castCredit ->
+                        ListItem(
+                            headlineContent = { Text(text = castCredit.embedded.movie.name) },
+                            leadingContent = {
+                                Box {
+                                    FlickAsyncImage(
+                                        url = castCredit.embedded.movie.image.medium,
+                                        contentDescription = "Movie image",
+                                        modifier = Modifier
+                                            .clip(RoundedCornerShape(12.dp))
+                                            .size(56.dp),
+                                        contentScale = ContentScale.Crop
+                                    )
+                                }
+                            },
+                            supportingContent = { Text(text = castCredit.embedded.character.name) }
+                        )
+                    }
+                }
+            }
         }
-
-
     }
 }
