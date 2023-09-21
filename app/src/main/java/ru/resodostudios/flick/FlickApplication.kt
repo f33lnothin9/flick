57389a9c5934ad96cfad028e9e0e42ade8a9026b
Nikita Bulavin <f33lnothin9@proton.me>
2023-09-21 20:28:38 +0300

    Init mobile ads

--- app/src/main/java/ru/resodostudios/flick/FlickApplication.kt
+++ app/src/main/java/ru/resodostudios/flick/FlickApplication.kt
@@ -1,7 +1,14 @@
 package ru.resodostudios.flick
 
 import android.app.Application
+import com.yandex.mobile.ads.common.MobileAds
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FlickApplication : Application()
+class FlickApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        MobileAds.initialize(this) { }
+    }
+}
