b80f90eb81a19b6dbf08b44686f9cf05929d963d
Nikita Bulavin <f33lnothin9@proton.me>
2023-10-23 21:53:00 +0300

    Collect userdata uiState

--- app/src/main/java/ru/resodostudios/flick/MainActivity.kt
+++ app/src/main/java/ru/resodostudios/flick/MainActivity.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import ru.resodostudios.flick.MainActivityUiState.Loading
 import ru.resodostudios.flick.core.data.util.NetworkMonitor
 import ru.resodostudios.flick.core.designsystem.theme.FlickTheme
 import ru.resodostudios.flick.ui.FlickApp
@@ -20,9 +31,24 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var networkMonitor: NetworkMonitor
 
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        var uiState: MainActivityUiState by mutableStateOf(Loading)
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState
+                    .onEach {
+                        uiState = it
+                    }
+                    .collect()
+            }
+        }
+
         enableEdgeToEdge()
         setContent {
             FlickTheme {
