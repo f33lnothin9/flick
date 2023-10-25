8c33d096b0cbcabc27b64360928af00b1c9fb0dc
Nikita Bulavin <f33lnothin9@proton.me>
2023-10-25 23:11:48 +0300

    Rename rootProject.name to flick

--- app/src/androidTest/kotlin/ru/resodostudios/flick/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/ru/resodostudios/flick/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
 package ru.resodostudios.flick
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ru.resodostudios.movies", appContext.packageName)
+        assertEquals("ru.resodostudios.flick", appContext.packageName)
     }
 }
