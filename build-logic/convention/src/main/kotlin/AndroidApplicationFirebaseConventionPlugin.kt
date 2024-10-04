0101d0de3cde2d37d014f1d54dc67b7a51a364ff
Nikita Bulavin <f33lnothin9@proton.me>
2024-10-04 10:59:38 +0300

    Add firebase performance

--- build-logic/convention/src/main/kotlin/AndroidApplicationFirebaseConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationFirebaseConventionPlugin.kt
@@ -1,16 +1,15 @@
-import com.android.build.api.dsl.ApplicationExtension
-import com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension
-import ru.resodostudio.flick.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import ru.resodostudio.flick.libs
 
 class AndroidApplicationFirebaseConventionPlugin : Plugin<Project> {
+
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
                 apply("com.google.gms.google-services")
+                apply("com.google.firebase.firebase-perf")
                 apply("com.google.firebase.crashlytics")
             }
 
@@ -18,15 +17,8 @@ class AndroidApplicationFirebaseConventionPlugin : Plugin<Project> {
                 val bom = libs.findLibrary("firebase-bom").get()
                 add("implementation", platform(bom))
                 "implementation"(libs.findLibrary("firebase.analytics").get())
+                "implementation"(libs.findLibrary("firebase.performance").get())
                 "implementation"(libs.findLibrary("firebase.crashlytics").get())
-            }
-
-            extensions.configure<ApplicationExtension> {
-                buildTypes.configureEach {
-                    configure<CrashlyticsExtension> {
-                        mappingFileUploadEnabled = false
-                    }
-                }
             }
         }
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,13 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.compose) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.firebase.perf) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.gms) apply false
-    alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.protobuf) apply false
 }
